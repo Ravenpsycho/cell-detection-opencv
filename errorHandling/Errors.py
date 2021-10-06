a092218f6cfe0718ef8fc821558e73d9ef33fbc1
Mat <mathieu.canales@protonmail.com>
2021-10-06 13:48:48 +0200

    OOP version

--- errorHandling/Errors.py
+++ errorHandling/Errors.py
@@ -1,3 +1,13 @@
 class DirNotMatchingError(Exception):
     """Raised when the urls of both files are not in the same directory"""
     pass
+
+
+class ListLengthError(Exception):
+    """Raised if the length of two lists are different"""
+    pass
+
+
+class MisMatchError(Exception):
+    """Raised if the length of two lists are different"""
+    pass
