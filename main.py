a887de65775a1d9df816fcb62c6c8d756be272f2
Mat <mathieu.canales@protonmail.com>
2021-10-06 15:34:29 +0200

    OOP version, better examples in img

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import pandas as pd
 import re
 
 from errorHandling.Errors import DirNotMatchingError, ListLengthError, MisMatchError
@@ -10,12 +11,16 @@ from tkinter.filedialog import askdirectory
 
 
 class OutlierRemover:
-    def __init__(self, pattern_dapi, pattern_other, factor=10,):
+    def __init__(self, pattern_dapi, pattern_other, factor=10, score=True, verbose=False):
         self.pat_dapi = pattern_dapi
         self.pat_other = pattern_other
         self.factor = factor
+        self.score = score
+        self.verbose = verbose
+
         self.list_dapi = []
         self.list_other = []
+        self.score_df = pd.DataFrame(columns=['file', 'dapi_score', f'{self.pat_other}_score'])
 
     def remove_outliers(self, dapi_url, other_channel_url):
         # split path and set dir
@@ -34,23 +39,26 @@ class OutlierRemover:
         img2 = cv2.imread(other_channel_url)
 
         # show
-        cv2.imshow('original image DAPI', img)
-        cv2.waitKey(0)
-        cv2.imshow('original image to apply mask on', img2)
-        cv2.waitKey(0)
+        if self.verbose:
+            cv2.imshow('original image DAPI', img)
+            cv2.waitKey(0)
+            cv2.imshow('original image to apply mask on', img2)
+            cv2.waitKey(0)
 
         # Convert to grayscale
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Blur the image for better edge detection
         blur = cv2.GaussianBlur(img_gray, (5, 5), cv2.BORDER_DEFAULT)
-        cv2.imshow('blurred image', blur)
-        cv2.waitKey(0)
+        if self.verbose:
+            cv2.imshow('blurred image', blur)
+            cv2.waitKey(0)
 
         # use threshold to have a 1_0 cut between edges and background
-        ret3, th3 = cv2.threshold(blur, 150, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        cv2.imshow('th3', th3)
-        cv2.waitKey(0)
+        ret3, th3 = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        if self.verbose:
+            cv2.imshow('th3', th3)
+            cv2.waitKey(0)
 
         # use findContours to delimit the shapes, also allows to have the size of the contours
         _, contours, hierarchy = cv2.findContours(image=th3, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
@@ -59,8 +67,9 @@ class OutlierRemover:
         # OPTIONAL: plot using drawContours on a copy of the original image
         cv2.drawContours(plot_img, contours, contourIdx=-1, color=(0, 255, 0),
                          thickness=3, lineType=cv2.FILLED)
-        cv2.imshow('contours of ALL structures', plot_img)
-        cv2.waitKey(0)
+        if self.verbose:
+            cv2.imshow('contours of ALL structures', plot_img)
+            cv2.waitKey(0)
 
         # the size is captured in an array using contourArea and list comprehension
         size_list = np.array([cv2.contourArea(x) for x in contours])
@@ -74,8 +83,9 @@ class OutlierRemover:
         # again, plotting the big structures using drawContours
         plot_img = img.copy()
         cv2.drawContours(plot_img, contours_big, -1, (0, 255, 0), 3, cv2.LINE_AA)
-        cv2.imshow('contours of big structures', plot_img)
-        cv2.waitKey(0)
+        if self.verbose:
+            cv2.imshow('contours of big structures', plot_img)
+            cv2.waitKey(0)
 
         # initializing a full white mask (255 values across a np array the same size as our image)
         mask = np.zeros(img.shape, np.uint8) + 255
@@ -85,13 +95,35 @@ class OutlierRemover:
 
         # applying the mask on the second picture
         mask_applied = cv2.bitwise_and(mask, img2)
-        cv2.imshow('mask applied', mask_applied)
-        cv2.waitKey(0)
+        if self.verbose:
+            cv2.imshow('mask applied', mask_applied)
+            cv2.waitKey(0)
 
         # output in the subdir
         m = re.match(r'(.*)(\..*)', other_path[1])
-        filepath = output_dir + "/" + m.group(1) + '_outliers_removed' + m.group(2)
+        filepath = output_dir + "/" + m.group(1) + f'_outliers_removed_({len(contours_big)})' + m.group(2)
         cv2.imwrite(filepath, mask_applied)
+
+        print(mean_size)
+
+        # output the scoring
+        if self.score:
+            score_other_img = cv2.cvtColor(mask_applied, cv2.COLOR_BGR2GRAY)
+            score_dapi_img = cv2.cvtColor(cv2.bitwise_and(mask, img), cv2.COLOR_BGR2GRAY)
+            dapi_values = [score_dapi_img[i, j]
+                           for i in range(score_dapi_img.shape[0])
+                           for j in range(score_dapi_img.shape[1])
+                           ]
+            other_values = [score_other_img[i, j]
+                            for i in range(score_other_img.shape[0])
+                            for j in range(score_other_img.shape[1])
+                            ]
+
+            self.score_df = self.score_df.append({
+                'file': other_path[1],
+                'dapi_score': np.array(dapi_values).mean(),
+                f'{self.pat_other}_score': np.array(other_values).mean()
+            }, ignore_index=True)
 
     def find_matches(self):
         path = askdirectory(title='Select a Folder', initialdir='.')
@@ -116,9 +148,13 @@ class OutlierRemover:
         self.list_other += [path + "/" + f for f in other_files]
 
         for i in range(len(self.list_dapi)):
+            print(f'processing image {i} of {len(self.list_dapi)}')
             self.remove_outliers(self.list_dapi[i], self.list_other[i])
+
+        if self.score:
+            self.score_df.to_csv(path + "/outliers_removed/scoring.csv")
 
 
 if __name__ == '__main__':
-    outlier_finder = OutlierRemover('DAPI', 'colorName')
+    outlier_finder = OutlierRemover('d1', 'd0')
     outlier_finder.find_matches()
